836eb2b270a7930a87dd592034477c0245d37e40
kittygirl101010 <lydia@xmitter.net>
2020-11-25 11:37:01 -0800

    i did something!!!

---
+++ code/PizzaCalculator.py
@@ -0,0 +1,8 @@
+#PizzaCalculator
+
+
+Number_Of_Pizzas = eval(input("how many pizzas do you want? "))
+cost_For_Pizza=eval(input("how much dose each pizza cost? "))
+subtotal = Number_Of_Pizzas * cost_For_Pizza
+
+print("total cost is $",subtotal)
